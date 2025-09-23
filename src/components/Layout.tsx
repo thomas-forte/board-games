17c82de2c299835b018ab3647e22f65509699200
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-09-23 00:34:41 -0400

    links no menu

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,31 +1,57 @@
-import { NavLink, Outlet, useLocation } from "react-router";
+import { Link, Outlet, useLocation } from "react-router";
+import { ArrowLeftIcon, LifebuoyIcon } from "@heroicons/react/24/outline";
 
 export const Layout = () => {
   const location = useLocation();
 
+  const getBackLink = () => {
+    const [, , tag, game] = location.pathname.split("/");
+    if (game && tag) {
+      return (
+        <Link to={`/category/${tag}`}>
+          <div className="inline-flex items-center gap-2">
+            <ArrowLeftIcon className="size-6" />
+            {tag.replaceAll("_", " ")}
+          </div>
+        </Link>
+      );
+    } else if (tag) {
+      return (
+        <Link to="/">
+          <div className="inline-flex items-center gap-2">
+            <ArrowLeftIcon className="size-6" /> Home
+          </div>
+        </Link>
+      );
+    } else {
+      return (
+        <Link to="/old">
+          <div className="inline-flex items-center gap-2">
+            <LifebuoyIcon className="size-6" /> Old
+          </div>
+        </Link>
+      );
+    }
+  };
+
+  const getCurrentLinkText = () => {
+    const [, , tag, game] = location.pathname.split("/");
+    if (game && tag) {
+      return "Details";
+    } else if (tag) {
+      return tag.replaceAll("_", " ");
+    } else {
+      return "Home";
+    }
+  };
+
   return (
     <>
       <nav className="w-full flex justify-between font-lato font-light uppercase text-2xl">
-        {location.pathname === "/" ? (
-          <NavLink
-            to="/old"
-            className={({ isActive }) =>
-              isActive ? "underline underline-offset-4" : ""
-            }
-          >
-            Old
-          </NavLink>
-        ) : (
-          <div></div>
-        )}
-        <NavLink
-          to="/"
-          className={({ isActive }) =>
-            isActive ? "underline underline-offset-4" : ""
-          }
-        >
-          Home
-        </NavLink>
+        {getBackLink()}
+        <div className="underline underline-offset-4">
+          {getCurrentLinkText()}
+        </div>
       </nav>
       <Outlet />
     </>
