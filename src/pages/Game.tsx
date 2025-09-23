6e1d46c6324d213b8855f58e0a6a9f31bb36c1e7
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-09-22 22:40:09 -0400

    game page mock 1

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -2,6 +2,10 @@ import { useParams } from "react-router";
 
 import gamesData from "../assets/scraped.json";
 import type { Game as GameType } from "../types/game";
+import { Card } from "../components/Card";
+
+import bggLogo from "../assets/bgg_logo.svg";
+import nkLogo from "../assets/nk_logo.png";
 
 export const Game = () => {
   const { gameId } = useParams();
@@ -16,21 +20,67 @@ export const Game = () => {
 
   return (
     <>
-      <h1 className="text-[4rem] font-semibold font-oswald text-primary uppercase text-center mt-8">
+      <h1 className="text-[2rem] font-semibold font-oswald text-primary uppercase text-center mt-8">
         {game.name}
       </h1>
-      <p>{game.publishers.join(", ")}</p>
-      <p>{game.releaseYear}</p>
-      <p>{game.bggRating}</p>
-      <p>{game.players.min}</p>
-      <p>{game.players.max}</p>
-      <p>{game.players.best}</p>
-      <p>{game.playtime.min}</p>
-      <p>{game.playtime.max}</p>
-      <p>{game.complexityRating}</p>
-      <p>{game.bggUrl}</p>
-      <p>{game.nobleKnightUrl}</p>
-      <p>{game.tags.join(", ")}</p>
+
+      {game.publishers.length > 1 && (
+        <h2 className="text-[1.5rem] font-lato text-primary-neutral uppercase text-center mt-4">
+          {game.publishers[0]}
+        </h2>
+      )}
+
+      <h3 className="font-lato text-primary-neutral uppercase text-center">
+        {game.releaseYear}
+      </h3>
+
+      <h4 className="text-[.875rem] font-lato text-primary-neutral uppercase text-center">
+        game type
+      </h4>
+
+      <Card>
+        <p>{game.bggRating}</p>
+        <p>{game.complexityRating}</p>
+      </Card>
+
+      <div className="grid grid-cols-2 gap-4 mt-4">
+        <div>
+          <p>{game.playtime.min}</p>
+          <p>{game.playtime.max}</p>
+        </div>
+        <div>
+          <p>{game.players.min}</p>
+          <p>{game.players.max}</p>
+          <p>{game.players.best}</p>
+        </div>
+        <div>counters</div>
+        <div>maps</div>
+        <div>counter size</div>
+        <div>scenarios</div>
+      </div>
+      <div>cost</div>
+
+      <div className="flex justify-around items-center gap-4">
+        <a
+          className="hover:opacity-75"
+          href={game.bggUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="h-8" src={bggLogo} alt="Board Game Geek" />
+        </a>
+        {/* if game link is not empty */}
+        <a
+          className="hover:opacity-75"
+          href={game.nobleKnightUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img className="h-8" src={nkLogo} alt="Noble Knight" />
+        </a>
+      </div>
+
+      <div>rules</div>
     </>
   );
 };
