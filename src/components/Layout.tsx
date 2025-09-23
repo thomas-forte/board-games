3e1d2f79f1a11cc2a657469c7a44b67300a88b1b
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-09-22 21:35:32 -0400

    only show the old link on the main page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,23 @@
-import { NavLink, Outlet } from "react-router";
+import { NavLink, Outlet, useLocation } from "react-router";
 
 export const Layout = () => {
+  const location = useLocation();
+
   return (
     <>
       <nav className="w-full flex justify-between font-lato font-light uppercase text-2xl">
-        <NavLink
-          to="/old"
-          className={({ isActive }) =>
-            isActive ? "underline underline-offset-4" : ""
-          }
-        >
-          Old
-        </NavLink>
+        {location.pathname === "/" ? (
+          <NavLink
+            to="/old"
+            className={({ isActive }) =>
+              isActive ? "underline underline-offset-4" : ""
+            }
+          >
+            Old
+          </NavLink>
+        ) : (
+          <div></div>
+        )}
         <NavLink
           to="/"
           className={({ isActive }) =>
