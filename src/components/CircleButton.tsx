cccc35b2182b697fdb1082c27c5bff3f955b9905
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-09-22 20:47:20 -0400

    circle button updates

--- src/components/CircleButton.tsx
+++ src/components/CircleButton.tsx
@@ -1,19 +1,28 @@
+import { useNavigate } from "react-router";
+
 export const CircleButton = ({
   className,
   key,
   text,
+  to,
 }: {
   className?: string;
-  key: string;
+  key?: string;
   text: string;
+  to?: string;
 }) => {
+  const navigate = useNavigate();
+
   return (
     <div
       key={key}
-      className={`flex flex-col items-center circle-button ${className}`}
+      className={`p-4 flex flex-col items-center circle-button ${className}`}
     >
-      <button className="rounded-full size-22 cursor-pointer hover:outline-1 hover:-outline-offset-1 hover:outline-gray-300"></button>
-      <span className="text-sm font-lato font-normal">{text}</span>
+      <button
+        className="rounded-full size-22 cursor-pointer hover:outline-1 hover:-outline-offset-1 hover:outline-gray-300"
+        onClick={() => to && navigate(to)}
+      ></button>
+      <span className="text-sm font-lato font-normal text-center">{text}</span>
     </div>
   );
 };
