6616e6078b79dc1c592b793ea75efae36a70bf24
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-10-14 20:09:50 -0400

    better rating

--- src/components/game/GameRating.tsx
+++ src/components/game/GameRating.tsx
@@ -5,8 +5,14 @@ import { GameRatingComplexity } from "./GameRatingComplexity";
 
 export const GameRating = ({ game }: { game: Game }) => {
   return (
-    <Card className="mt-4 py-2 px-10 w-[20rem] w-max-content mx-auto">
-      <p>{game.bggRating}</p>
+    <Card className="mt-4 py-2 px-10 w-[20rem] w-max-content mx-auto font-lato">
+      <div className="my-4">
+        <div className="text-center text-[1.5rem] leading-[1.5rem]">
+          {game.bggRating.toFixed(2)}
+          <span className="text-[1rem] leading-[1rem]">/10</span>
+        </div>
+        <div className="text-center uppercase text-[.625rem]">Avg. Rating</div>
+      </div>
 
       <GameRatingComplexity game={game} />
     </Card>
