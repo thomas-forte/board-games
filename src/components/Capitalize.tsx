cf2d85acab3c65971fbae5a9b758f675ace79a13
Thomas Forte <5710966+thomas-forte@users.noreply.github.com>
2025-10-15 20:39:18 -0400

    remove logger

--- src/components/Capitalize.tsx
+++ src/components/Capitalize.tsx
@@ -1,6 +1,5 @@
 export const CapitalizedText = ({ text }: { text: string }) => {
   return text.split("").map((char, index) => {
-    console.log(char);
     if (char === char.toUpperCase() && char.match(/[A-Z]/i)) {
       return (
         <span key={index} className="text-[1.25em]">
